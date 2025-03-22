c7787268f8a12c20a08563336d25b93de76f8282
SUMITHAA P V R <sumithaapvr.22cse@kongu.edu>
2025-03-22 12:15:17 +0530

    Add files via upload

---
+++ DAY - 03/Source Code/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+    echo hello
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u sumithaapvr -p dckr_pat_5mSrlWhvl5iRd7YeNheximdIMDA
+    docker tag test1 sumithaapvr/newrepo1
+    docker push sumithaapvr/newrepo1
+    docker-compose up -d
+    
